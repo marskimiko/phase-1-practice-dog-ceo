50bf036648a921d39e5fd5571a4c918431ffd27e
marisasaunders <marisaksaunders@gmail.com>
2022-02-02 09:13:50 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ console.log('%c HI', 'color: firebrick')
 const container = document.querySelector("#dog-image-container")
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+let dog_list = [];
+
+const dropdown = document.querySelector("#breed-dropdown")
+dropdown.addEventListener('change', handleChange)
 
 
 function getImages() {
@@ -23,7 +27,7 @@ getImages();
 
 
 function getDogBreed() {
-  let dog_list = [];
+  // let dog_list = [];
   const dogBreeds = document.getElementById('dog-breeds');
   fetch(breedUrl)
     .then(response => {
@@ -37,16 +41,53 @@ function getDogBreed() {
         let node = document.createTextNode(dog);
         li.appendChild(node);
         dogBreeds.appendChild(li);
+        li.addEventListener('click', () => {
+          li.style.color = 'blue';
+        })
       }
     });
-  dogBreeds.addEventListener('click')
 }
 getDogBreed();
 
+function handleChange(event) {
+  debugger;
+  const letter = event.target.value
+  //console.log(dog_list)
+  const filteredBreed = dog_list.filter(breed => breed.startsWith(letter));
+
+  // filter out hte breeds that start with the letter
+}
+
+handleChange();
+
+// ## Challenge 4
+
+// Once we are able to load _all_ of the dog breeds onto the page, add JavaScript
+// so that the user can filter breeds that start with a particular letter using a
+// [dropdown](https://www.w3docs.com/learn-html/html-select-tag.html).
+
+// For example, if the user selects 'a' in the dropdown, only show the breeds with
+// names that start with the letter a. For simplicity, the dropdown only includes
+// the letters a-d. However, we can imagine expanding this to include the entire
+// alphabet.
 
 
-// ## Challenge 3
+// function myFunction() {
+//   document.getElementById("breed-dropdown").classList.toggle("show");
+// }
 
-// Once all of the breeds are rendered in the `<ul>`, add JavaScript so that, when
-// the user clicks on any one of the `<li>`s, the font color of that `<li>`
-// changes. This can be a color of your choosing.
+// function filterFunction() {
+//   var input, filter, ul, li, a, i;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   div = document.getElementById("breed-dropdown");
+//   a = div.getElementsByTagName("a");
+//   for (i = 0; i < a.length; i++) {
+//     txtValue = a[i].textContent || a[i].innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       a[i].style.display = "";
+//     } else {
+//       a[i].style.display = "none";
+//     }
+//   }
+// }
